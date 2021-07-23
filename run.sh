407df59f60a1c6cff29724caf77611acf4fd3922
DYGV <11eisuke88@gmail.com>
2021-07-24 02:37:04 +0900

    change files

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@ trap "exit 1" 2
 for i in {1..10}
 do
   echo "${i} --------------------------------------------------------"
-  perf stat -e instructions,cache-references,cache-misses ./serial_omp_vec_time
+  perf stat -e instructions,cache-references,cache-misses ./serial_omp_vec_time &> /dev/null
   if [ $? -gt 0 ]; then
-    exit 1
+    ./serial_omp_vec_time
   fi
 done
